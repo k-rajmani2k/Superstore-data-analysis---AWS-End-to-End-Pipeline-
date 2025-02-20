3ddccbe8b3f3821797ae4bdda1e23b509d1accf5
Kumar Rajmani Bapat <k.rajmani2k@gmail.com>
2025-02-20 04:37:38 -0500

    Update AthenaQuery2.sql

--- AthenaQuery2.sql
+++ AthenaQuery2.sql
@@ -1,3 +1,13 @@
 -- To get the category-wise sum of sales for each product
 SELECT category,sum(sales) as category_sales FROM "db_salesproduct"."orders"
 group by category
+
+
+-- ouput 
+/*
+#	category	category_sales
+1	Office Supplies	36346.47999999999
+2	Technology	29116.44
+3		
+4	Furniture	15029.990099999999
+*/
